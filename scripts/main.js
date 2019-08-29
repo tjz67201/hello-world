f5a694c96175a5a48c0a29dfc224059b6757803e
tjz67201 <54682345+tjz67201@users.noreply.github.com>
2019-08-29 15:26:33 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/img1.jpg') {
+      myImage.setAttribute ('src','images/img2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/img1.jpg');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
